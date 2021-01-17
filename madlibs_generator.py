3517750769bb91ab9ed43198281b244e20a36f7d
Kavyashree <69260342+kavyashree-205@users.noreply.github.com>
2021-01-17 21:06:09 +0530

    Add files via upload

---
+++ madlibs_generator.py
@@ -0,0 +1,22 @@
+name=input("Enter a male name : ")
+
+occupation=input("Enter occupation : ")
+
+age=input("Enter age : ")
+
+place=input("Enter place name : ")
+
+place1=input("Enter another place name : ")
+
+verb=input("Enter verb : ")
+
+verb1=input("Enter another verb : ")
+
+verb2=input("Enter another verb : ")
+
+days=input("Enter the number of days : ")
+
+print("\n")
+
+madlibs=f"There is a man named {name},he is a {occupation}.\nHis age is {age}. He lives in a very weird place called {place}.\nHe has no time for {verb}ing. He can never {verb1} because he has malignant tumor.\nHe just have {days} days to live. His biggest dream is {verb2}ing with aliens and dancing at {place1}."
+print(madlibs)
